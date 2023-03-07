69570079da600b79ba03cd1d60f2970999984980
Grzegorz Smoktunowicz <biarth@wp.pl>
2023-03-07 19:54:37 +0100

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
+import { ModalService } from '../services/modal.service';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
+import { EventBlockerDirective } from './directives/event-blocker.directive';
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+    EventBlockerDirective,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective, NgxMaskPipe],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+    AlertComponent,
+    EventBlockerDirective,
+  ],
+  providers: [ModalService, provideNgxMask()],
+})
+export class SharedModule {}
